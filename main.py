c5880953d4e7ea5658f1d20fbaf56fccf218d1a7
Peter Rucker <peter@immunoconcepts.com>
2021-04-23 07:59:43 -0700

    added 3-way and 4-way merge

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.merge import two_way_merge
+from src.merge import two_way_merge, four_way_merge, three_way_merge
 import os
 source1 = "src/images/Case_1019/complaint"
 source1label = ""
@@ -6,16 +6,18 @@ source2 = "src/images/Case_1019/jackson"
 source2label = ""
 destination = "top_row"
 
+#four_way_merge("src/images/one","one","src/images/two","two","src/images/three","three","src/images/four","four","Final")
+three_way_merge("src/images/one","Position One - Neg Control","src/images/two","Position two - Neg Control","src/images/three","Position Three - SSA Control","Final")
 
 #four-way-merge
-source1,label1 = ("1x","1x 30ul wash")
-source2,label2 = ("20x","20x 30ul washes")
-source3,label3 = ("Heavy","5x 100ul washes")
-source4,label4 = ("Control","Control - 5x 30ul washes")
-
-print("first merge")
-two_way_merge("src/images/" + source1, label1, "src/images/" + source2, label2, source1 + source2)
-print("second merge")
-two_way_merge("src/images/" + source3, label3, "src/images/" + source4, label4, source3 + source4)
-print("Final merge")
-two_way_merge("src/images/" + source1 + source2, "", "src/images/" + source3 + source4, "", "Final", horizontal=False)
+# source1,label1 = ("1x","1x 30ul wash")
+# source2,label2 = ("20x","20x 30ul washes")
+# source3,label3 = ("Heavy","5x 100ul washes")
+# source4,label4 = ("Control","Control - 5x 30ul washes")
+#
+# print("first merge")
+# two_way_merge("src/images/" + source1, label1, "src/images/" + source2, label2, source1 + source2)
+# print("second merge")
+# two_way_merge("src/images/" + source3, label3, "src/images/" + source4, label4, source3 + source4)
+# print("Final merge")
+# two_way_merge("src/images/" + source1 + source2, "", "src/images/" + source3 + source4, "", "Final", horizontal=False)
--- src/merge.py
+++ src/merge.py
@@ -43,3 +43,85 @@ def two_way_merge(source1: str, source1label: str, source2: str, source2label: s
         if not os.path.isdir(f'src/images/{destination}/'):
             os.mkdir(f'src/images/{destination}/')
         new_image.save(f"src/images/{destination}/{filename}","JPEG",quality=95)
+
+def four_way_merge(source1,source1label,source2,source2label,source3,source3label,source4,source4label,destination):
+    folder1 = []
+    folder2 = []
+    folder3 = []
+    folder4 = []
+    myFont = ImageFont.truetype("src/arial.ttf", 60)
+    for filename in os.listdir(source1):
+        if filename.endswith((".jpg",".jpeg","png","gif")):
+            folder1.append(filename)
+    for filename in os.listdir(source2):
+        if filename.endswith((".jpg",".jpeg","png","gif")):
+            folder2.append(filename)
+    for filename in os.listdir(source3):
+        if filename.endswith((".jpg",".jpeg","png","gif")):
+            folder3.append(filename)
+    for filename in os.listdir(source4):
+        if filename.endswith((".jpg",".jpeg","png","gif")):
+            folder4.append(filename)
+    for file1,file2,file3,file4 in zip(folder1,folder2,folder3,folder4):
+        image1 = Image.open(source1 + "/" + file1)
+        d1 = ImageDraw.Draw(image1)
+        d1.text((28, 36), source1label, fill=(255, 255, 255), font=myFont)
+        dimensions = image1.size
+        print(dimensions)
+        image2 = Image.open(source2 + "/" + file2)
+        d2 = ImageDraw.Draw(image2)
+        d2.text((28, 36), source2label, fill=(255, 255, 255), font=myFont)
+        image3 = Image.open(source3 + "/" + file3)
+        d3 = ImageDraw.Draw(image3)
+        d3.text((28, 36), source3label, fill=(255, 255, 255), font=myFont)
+        image4 = Image.open(source4 + "/" + file4)
+        d4 = ImageDraw.Draw(image4)
+        d4.text((28, 36), source4label, fill=(255, 255, 255), font=myFont)
+        new_image = Image.new('RGB', (2 * dimensions[0], 2 * dimensions[1]), (255, 255, 255))
+        new_image.paste(image1, (0, 0))
+        new_image.paste(image2, (dimensions[0], 0))
+        new_image.paste(image3, (0, dimensions[1]))
+        new_image.paste(image4, (dimensions[0], dimensions[1]))
+        filename = file1
+        #new_image.show()
+        print(f"merging {file1},{file2},{file3},{file4}")
+        if not os.path.isdir(f'src/images/{destination}/'):
+            os.mkdir(f'src/images/{destination}/')
+        new_image.save(f"src/images/{destination}/{filename}", "JPEG", quality=95)
+
+def three_way_merge(source1,source1label,source2,source2label,source3,source3label,destination):
+    folder1 = []
+    folder2 = []
+    folder3 = []
+    myFont = ImageFont.truetype("src/arial.ttf", 60)
+    for filename in os.listdir(source1):
+        if filename.endswith((".jpg",".jpeg","png","gif")):
+            folder1.append(filename)
+    for filename in os.listdir(source2):
+        if filename.endswith((".jpg",".jpeg","png","gif")):
+            folder2.append(filename)
+    for filename in os.listdir(source3):
+        if filename.endswith((".jpg",".jpeg","png","gif")):
+            folder3.append(filename)
+    for file1,file2,file3 in zip(folder1,folder2,folder3):
+        image1 = Image.open(source1 + "/" + file1)
+        d1 = ImageDraw.Draw(image1)
+        d1.text((28, 36), source1label, fill=(255, 255, 255), font=myFont)
+        dimensions = image1.size
+        print(dimensions)
+        image2 = Image.open(source2 + "/" + file2)
+        d2 = ImageDraw.Draw(image2)
+        d2.text((28, 36), source2label, fill=(255, 255, 255), font=myFont)
+        image3 = Image.open(source3 + "/" + file3)
+        d3 = ImageDraw.Draw(image3)
+        d3.text((28, 36), source3label, fill=(255, 255, 255), font=myFont)
+        new_image = Image.new('RGB', (2 * dimensions[0], 2 * dimensions[1]), (30,30,30))
+        new_image.paste(image1, (0, 0))
+        new_image.paste(image2, (dimensions[0], 0))
+        new_image.paste(image3, (int(dimensions[0] / 2), dimensions[1]))
+        filename = file1
+        #new_image.show()
+        print(f"merging {file1},{file2},{file3}")
+        if not os.path.isdir(f'src/images/{destination}/'):
+            os.mkdir(f'src/images/{destination}/')
+        new_image.save(f"src/images/{destination}/{filename}", "JPEG", quality=95)
